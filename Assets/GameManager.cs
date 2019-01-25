92c1f6d460c3fee4f76d9680735ac12033b4ae53
BERKE KAGAN NOHUT <e2036135@ceng.metu.edu.tr>
2019-01-26 02:34:01 +0300

    NW

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public List<GameObject> Players = new List<GameObject>();
+
+    private void Awake()
+    {
+        if( Instance == null )
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] private bool _isPlayerOne ;
+    private bool _isConnectionSet;
+    private short movementmsg = 101;
+
+    public class CustomMessage : MessageBase
+    {
+        //public GameObject gobj;
+    }
+
+    private void Awake()
+    {
+        Application.runInBackground = true;
+        NetworkServer.RegisterHandler(movementmsg, OnServerReceivedMovementMessage);
+    }
+    void Update()
+    {   
+        if( isServer )
+        {   
+            if( !_isPlayerOne )
+            {
+                return;
+            }
+            
+            if (Input.GetMouseButton(0))
+            {
+                Movement(gameObject);
+            }
+        }
+        else if( !isServer )
+        {
+            if (_isPlayerOne)
+            {
+                return;
+            }
+            if (Input.GetMouseButton(0))
+            {
+                CustomMessage msg = new CustomMessage();
+                //msg.gobj = gameObject;
+                SendMovementCall(msg);
+            }
+        }
+    }
+    
+    void Movement(GameObject obj = null)
+    {   
+        if( obj == null )
+        {
+            obj = GameManager.Instance.Players[1];
+        }
+        Vector3 tmp = obj.transform.position;
+        tmp.y++;
+        obj.transform.position = tmp;
+    }
+    void SendMovementCall(CustomMessage msg)
+    {
+        NetworkClient client = NetworkManager.singleton.client;
+        client.Send(movementmsg, msg);
+    }
+    void OnServerReceivedMovementMessage(NetworkMessage msg)
+    {
+        //CustomMessage m = msg.ReadMessage<CustomMessage>();
+        Movement();
+    }
+}
