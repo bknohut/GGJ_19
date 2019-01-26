cf69c681ee8468cb972d06a88212ef6090613db1
BERKE KAGAN NOHUT <e2036135@ceng.metu.edu.tr>
2019-01-26 13:42:48 +0300

    House objects

---
+++ Assets/EntranceView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class EntranceView : MonoBehaviour
+{
+    [SerializeField] Transform _logo;
+    [SerializeField] Transform _playButton;
+
+    public void OnEnable()
+    {
+        _logo.DOLocalMoveY(390f, 1f).SetEase(Ease.InOutBounce);
+    }
+    public void OnStartButtonClicked()
+    {
+        _logo.DOLocalMoveY(800f, 1f).onComplete = () => gameObject.SetActive(false);
+    }
+}
