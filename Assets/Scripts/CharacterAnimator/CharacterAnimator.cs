76998227920605d05c0a93c3ff7cadace8fc5850
berkyasr <berkyasr@gmail.com>
2019-01-26 06:28:23 +0300

    Animation

---
+++ Assets/Scripts/CharacterAnimator/CharacterAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public Animator animator;
+
+    public void Turn()
+    {
+
+    }
+
+    public void RunAnimation()
+    {
+        animator.SetTrigger("Run");
+    }
+
+    public void IdleAnimation()
+    {
+        animator.SetTrigger("Idle");
+    }
+
+    public void Smoke0Animation()
+    {
+        animator.SetTrigger("Smoke0");
+    }
+
+    public void Smoke1Animation()
+    {
+        animator.SetTrigger("Smoke1");
+    }
+}
---
+++ Assets/Scripts/CharacterAnimator/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool run;
+    private bool oldRun;
+    public Animator animator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (run != oldRun)
+        {
+            if (run)
+                animator.SetTrigger("Run");
+            else
+                animator.SetTrigger("Idle");
+            oldRun = run;
+        }
+        
+    }
+}
