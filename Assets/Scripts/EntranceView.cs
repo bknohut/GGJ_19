855da27cba9e42f4dd77c15401a3ce7dfd3d316a
BERKE KAGAN NOHUT <e2036135@ceng.metu.edu.tr>
2019-01-27 12:46:10 +0300

    Mother commit.

--- Assets/Scripts/EntranceView.cs
+++ Assets/Scripts/EntranceView.cs
@@ -9,6 +9,8 @@ public class EntranceView : MonoBehaviour
 
     [SerializeField] Transform _logo;
     [SerializeField] Transform _playButton;
+    [SerializeField] Transform _motherEntrance;
+    [SerializeField] GameObject _background;
 
     public void OnEnable()
     {
@@ -16,9 +18,22 @@ public class EntranceView : MonoBehaviour
     }
     public void OnStartButtonClicked()
     {
-        _logo.DOLocalMoveY(800f, .5f).SetEase(Ease.InQuad).onComplete = () => 
-                                {
-                                    GameStarted?.Invoke();
-                                    gameObject.SetActive(false); };
-                                }
+        _logo.DOLocalMoveY(800f, .5f).SetEase(Ease.InQuad);
+        _motherEntrance.DOLocalMoveX(0, .5f).SetEase(Ease.OutQuad).onComplete = () =>
+        {
+            StartCoroutine(Wait());
+            _background.SetActive(false);
+            _playButton.gameObject.SetActive(false);
+        };
+    }
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2f);
+
+        _motherEntrance.DOLocalMoveX(1920f, .5f).SetEase(Ease.InQuad).onComplete = () =>
+        {
+            GameStarted?.Invoke();
+        };
+    }
+        
 }
